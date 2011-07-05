d36f619089ee0d1f610b64c32b0af39648ecfb71
jicheng.li <deemstone@gmail.com>
2011-07-05 17:37:42 +0800

    不行了,大改造!!

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
 var http = require('http');
 var sys  = require('sys');
-var config = require('./config.js');
 
-var local = require('./local.js');  //过滤器,匹配指定的本地文件
-var remote = require('./remote.js');  //过滤器 匹配其他测试服务器
-var online = require('./online.js');  //这是整个过滤流程的最后一步
+var sifter = require('./sifter.js');
 
-config.init();
+var online = require('./methods/online.js');  //这是整个过滤流程的最后一步
 
-var filters = [local, remote, online]
-http.createServer(function(request, response) {
-	sys.log('--> : init');
+//初始化列表维护相关服务
+sifter.init();
 
-	//按照地址,检查应该从哪里取文件
-	var rs = config.testURL(request.url);
-	if(rs.domain){
-		if(rs.domain == 'local'){
-			//直接local取文件
-			local.serve(rs, response);
-		}else{
-			//构造请求head 从其他服务器请求文件
-			remote.serve(rquest, response, rs);
-		}
-	}else{
-		//正常代理 从线上取文件
-		online.serve(request, response);
-	}
-}).listen(80);
+//启动服务
+var server = http.createServer(function(request, response) {
+	sys.log('--> : init the http server');
+	//check if this request is listed in the sifter
+	if( sifter.check(request, response) ) return;
+	
+	//not shot the list , get it frome online
+	online.serve(request, response);
+	return true;
+
+});
+server.listen(80);
+
+sys.log('--> : Proxy Server listening port 80 !!');
